77f58b8b158b0bb2b85636a3b20edb929f672166
Masiluve Gidimi <masiluvegidimi@gmail.com>
2026-04-02 19:03:56 +0200

    Fix server path

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 
-
 import aiRoutes from "./routes/aiRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import resumeRoutes from "./routes/resumeRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -13,15 +13,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 mongoose.connect(process.env.DATABASE_URL)
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB connection error:", err));
-
+  .catch(err => console.error(err));
 
 app.use("/api/ai", aiRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/resume", resumeRoutes);
 
+app.get("/", (req, res) => {
+  res.send("API running 🚀");
+});
 
 app.use((req, res) => {
   res.status(404).json({
@@ -29,6 +31,4 @@ app.use((req, res) => {
   });
 });
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(process.env.PORT || 5000);
