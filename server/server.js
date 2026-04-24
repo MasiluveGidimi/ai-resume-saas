1e5bc4d6a281f5d51593e9e45f10bb0c4c6f8d7d
Masiluve Gidimi <masiluvegidimi@gmail.com>
2026-04-24 15:17:45 +0200

    connect frontend to live backend

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ app.use(express.json());
 
 app.use("/api/resume", resumeRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
